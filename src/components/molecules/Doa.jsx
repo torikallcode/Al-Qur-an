b7f60f98891cfc5faf5e19df59c853ea3146a2c0
torikallcode <torikalcontent@gmail.com>
2024-03-17 18:23:43 +0800

    detail doa done

--- src/components/molecules/Doa.jsx
+++ src/components/molecules/Doa.jsx
@@ -4,9 +4,11 @@ import { Link } from 'react-router-dom'
 export const Doa = ({ judul, className, id }) => {
   return (
     <Link to={`/doa/${id}`}>
-      <div className={`grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-3 w-full gap-x-7 border-b shadow-sm ${className}`}>
-        <div className="w-full py-5 px-3">
-          <h1 className="text-center font-inter text-lg font-semibold text-primary">{judul}</h1>
+      <div className={`flex justify-center items-center gap-y-3 w-full gap-x-5 md:gap-x-0 border-b shadow-sm ${className}`}>
+        <div className="w-full py-5 px-3 flex items-center gap-x-3">
+          <h1 className='font-inter font-semibold text-primary text-lg'>{id}.</h1>
+          <br />
+          <h1 className="font-inter text-lg font-semibold text-primary">{judul}</h1>
         </div>
       </div>
     </Link>
--- src/components/organism/detailDoa/index.jsx
+++ src/components/organism/detailDoa/index.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { getDetailDoa } from '../../../sevices/Doa'
+import { BackSvg } from '../../atoms/BackSvg'
 
 export const DetailDoa = () => {
   const { id } = useParams()
@@ -14,13 +15,20 @@ export const DetailDoa = () => {
   console.log(doa)
 
   return (
-    <div>
+    <div className='w-full flex justify-center items-center'>
       {doa ? (
-        <div key={doa.id}>
-          <h1>{doa.judul}</h1>
-          <h1>{doa.latin}</h1>
-          <h1>{doa.arab}</h1>
-          <h1>{doa.terjemah}</h1>
+        <div key={doa.id} className='flex flex-col justify-center items-center gap-y-3 w-full h-[100dvh]'>
+          <div className='w-full bg-primary py-5 px-2 fixed top-0'>
+            <button className='absolute top-4 left-3  w-9 h-9' onClick={() => window.history.back()}>
+              <BackSvg className="w-full h-full" warna={'#EEEEEE'}></BackSvg>
+            </button>
+            <h1 className='text-center font-inter text-lg font-semibold text-putih uppercase'>{doa.judul}</h1>
+          </div>
+          <div className='px-4 max-w-xl flex flex-col justify-center items-center gap-y-2'>
+            <h1 className='text-center font-utama text-5xl'>{doa.arab}</h1>
+            <h1 className='font-inter text-center text-sm mb-5'>({doa.latin})</h1>
+            <h1 className='font-inter text-center font-medium'>terjemahan : <br /> "{doa.terjemah}"</h1>
+          </div>
         </div>
       ) : (
         <div className="w-full flex justify-center items-center h-[50vh]">
